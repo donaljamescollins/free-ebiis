1bb71644ad2a6177a82883c26231223487d1e598
donaljamescollins <donaljamescollins@gmail.com>
2023-04-01 12:23:19 -0700

    find distance from user to station with free ebike

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import urllib.request
 import json
+import geocoder
+from geopy import distance
 
 station_information_url = "https://gbfs.baywheels.com/gbfs/en/station_information.json"
 station_status_url = "https://gbfs.baywheels.com/gbfs/en/station_status.json"
@@ -31,8 +33,24 @@ for station in station_status_dict["data"]["stations"]:
         station["station_id"] in STATIONS_IN_SF
         and station["station_status"] == "active"
         and station["station_id"] not in ["74", "361", "572"]
+        and station["num_ebikes_available"] == station["num_bikes_available"]
     ):
-        if station["num_ebikes_available"] == station["num_bikes_available"]:
-            FREE_EBIKE_STATIONS.append(station["station_id"])
+        for id in station_information_dict["data"]["stations"]:
+            if id["station_id"] == station["station_id"]:
+                temp = {}
+                temp["name"] = id["name"]
+                temp["coords"] = str(id["lat"]) + ", " + str(id["lon"])
+                FREE_EBIKE_STATIONS.append(temp)
 
-print(FREE_EBIKE_STATIONS)
+# for name in FREE_EBIKE_STATIONS:
+#     print("http://maps.apple.com/?q=" + name.replace(" ", "+") + "&dirflg=w", "\n")
+
+
+user_geolocation = (geocoder.ip("me")).latlng
+
+# table where column 1 is station name, 2 is distance from me to the station and 3 is button to open apple maps directions to station
+
+for station in FREE_EBIKE_STATIONS:
+    station["distance"] = round(
+        distance.distance(user_geolocation, station["coords"]).miles, 2
+    )
