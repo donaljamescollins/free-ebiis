b122f77106fe520ec50244f539916b892ea3bfcf
donaljamescollins <donaljamescollins@gmail.com>
2023-04-01 20:23:04 -0700

    create index page

--- app.py
+++ app.py
@@ -3,55 +3,66 @@ import urllib.parse
 import json
 import geocoder
 from geopy import distance
+from flask import Flask, render_template
+from flask_session import Session
 
-station_information_url = "https://gbfs.baywheels.com/gbfs/en/station_information.json"
-station_status_url = "https://gbfs.baywheels.com/gbfs/en/station_status.json"
-
-station_information_json = urllib.request.urlopen(station_information_url)
-station_status_json = urllib.request.urlopen(station_status_url)
-
-station_information_dict = json.loads(
-    station_information_json.read()
-)  # contains station_id, name, lat, lon, region_id
-station_status_dict = json.loads(
-    station_status_json.read()
-)  # contains station_id, last_reported, num_ebikes_available, num_bikes_available, station_status
-
-STATIONS_IN_SF = []
-FREE_EBIKE_STATIONS = []
+app = Flask(__name__)
 
 
-for station in station_information_dict["data"]["stations"]:
-    if (
-        "region_id" in station
-        and station["region_id"] == "3"
-        and station["station_id"] not in ["74", "361", "572"]
-    ):
-        STATIONS_IN_SF.append(station["station_id"])
+@app.route("/")
+def index():
+    station_information_url = (
+        "https://gbfs.baywheels.com/gbfs/en/station_information.json"
+    )
+    station_status_url = "https://gbfs.baywheels.com/gbfs/en/station_status.json"
 
+    station_information_json = urllib.request.urlopen(station_information_url)
+    station_status_json = urllib.request.urlopen(station_status_url)
 
-for station in station_status_dict["data"]["stations"]:
-    if (
-        station["station_id"] in STATIONS_IN_SF
-        and station["station_status"] == "active"
-        and station["station_id"] not in ["74", "361", "572"]
-        and station["num_ebikes_available"] == station["num_bikes_available"]
-    ):
-        for id in station_information_dict["data"]["stations"]:
-            if id["station_id"] == station["station_id"]:
-                temp = {}
-                temp["name"] = id["name"]
-                temp["coords"] = str(id["lat"]) + ", " + str(id["lon"])
-                FREE_EBIKE_STATIONS.append(temp)
+    # contains station_id, name, lat, lon, region_id
+    station_information_dict = json.loads(station_information_json.read())
 
+    # contains station_id, last_reported, num_ebikes_available, num_bikes_available, station_status
+    station_status_dict = json.loads(station_status_json.read())
 
-user_geolocation = (geocoder.ip("me")).latlng
+    STATIONS_IN_SF = []
+    FREE_EBIKE_STATIONS = []
 
+    for station in station_information_dict["data"]["stations"]:
+        if (
+            "region_id" in station
+            and station["region_id"] == "3"
+            and station["station_id"] not in ["74", "361", "572"]
+        ):
+            STATIONS_IN_SF.append(station["station_id"])
 
-for station in FREE_EBIKE_STATIONS:
-    station["distance"] = distance.distance(user_geolocation, station["coords"]).miles
+    for station in station_status_dict["data"]["stations"]:
+        if (
+            station["station_id"] in STATIONS_IN_SF
+            and station["station_status"] == "active"
+            and station["station_id"] not in ["74", "361", "572"]
+            and station["num_ebikes_available"] == station["num_bikes_available"]
+        ):
+            for id in station_information_dict["data"]["stations"]:
+                if id["station_id"] == station["station_id"]:
+                    temp = {}
+                    temp["name"] = id["name"]
+                    temp["coords"] = str(id["lat"]) + ", " + str(id["lon"])
+                    FREE_EBIKE_STATIONS.append(temp)
 
-    station[
-        "maps_link"
-    ] = f"http://maps.apple.com/?daddr={urllib.parse.quote_plus(station['name'])}&dirflg=w"
-    print(station, "\n")
+    user_geolocation = (geocoder.ip("me")).latlng
+
+    for station in FREE_EBIKE_STATIONS:
+        station["distance"] = round(
+            distance.distance(user_geolocation, station["coords"]).miles, 2
+        )
+
+        station[
+            "maps_link"
+        ] = f"http://maps.apple.com/?daddr={urllib.parse.quote_plus(station['name'])}&dirflg=w"
+
+    return render_template(
+        "/index.html",
+        FREE_EBIKE_STATIONS=FREE_EBIKE_STATIONS,
+        user_geolocation=user_geolocation,
+    )
